c560319f7e477b889b14ca9e08cb5b2dc8691de0
Oleksandr SH <dp050791som@gmail.com>
2025-10-07 18:53:38 +0300

    HW1 commit 2

---
+++ HW2.py
@@ -0,0 +1,13 @@
+number_str = input("введите 4-х значное число: ")
+
+number = int(number_str)
+
+number1 = number // 1000
+number2 = (number % 1000) // 100
+number3 = (number % 100) // 10
+number4 = number % 10
+
+print(number1)
+print(number2)
+print(number3)
+print(number4)
