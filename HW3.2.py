7d8f114378cbede1689d5abb7108f38585a94323
Oleksandr SH <dp050791som@gmail.com>
2025-10-07 21:38:53 +0300

    HW3.2 DONE

---
+++ HW3.2.py
@@ -0,0 +1,7 @@
+lst = ['ddd', 10, 20, 30, 40, 50, 'Google']
+print(lst)
+x = lst.pop(-1)
+print("перемещаем в начало списака")
+print(x)
+lst.insert(0, x)
+print(lst)
---
+++ lession3.py
@@ -0,0 +1,10 @@
+orange_price = 15.5
+my_money = 13
+tea_price = 12
+if my_money > orange_price:
+    print("I buy orange")
+elif my_money > tea_price:
+    print ("Not orange, just tea")
+else:
+    print("I buy apple")
+print('OK')
