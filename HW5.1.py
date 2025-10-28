d4acf731649d29808fc4173ad94c4aadc298bffa
Oleksandr SH <dp050791som@gmail.com>
2025-10-28 15:36:16 +0200

    HW 5.1 done

---
+++ HW5.1.py
@@ -0,0 +1,23 @@
+import keyword
+import string
+
+name = input("введите значения строки: ")
+if name == "_":
+    print(True)
+    exit()
+if "__" in name:
+    print(False)
+    exit()
+if name in keyword.kwlist:
+    print(False)
+    exit()
+if name[0].isdigit():
+    print(False)
+    exit()
+if any(ch in string.punctuation.replace("_", "") or ch == " " for ch in name):
+    print(False)
+    exit()
+if any(ch.isupper() for ch in name):
+    print(False)
+    exit()
+print(True)
